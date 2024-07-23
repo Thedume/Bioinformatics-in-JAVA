1865ca3caec7c18cc7601584f5026e23d85637f0
진영 조 <jjojin97@gmail.com>
2024-07-23 10:38:06 +0900

    Add File

---
+++ Chap03Ex01.java
@@ -0,0 +1,8 @@
+// Ex. Chap03Ex01
+
+public class Chap03Ex01 {
+    public static void main(String[] args) {
+        String dna_seq = "acgtacgt";
+        System.out.println(dna_seq);
+    }
+}
---
+++ Chap03Ex02.java
@@ -0,0 +1,8 @@
+public class Chap03Ex02 {
+    public static void main(String[] args) {
+        String dna_seq = "acgtacgt";
+        int dna_len = dna_seq.length();
+        System.out.println("dna_seq : " + dna_seq);
+        System.out.println("Length of dna_seq : " + dna_len);
+    }
+}
