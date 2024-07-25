745d0f537d36042efcc670f8ae38f84293104bba
진영 조 <jjojin97@gmail.com>
2024-07-25 16:27:33 +0900

    Chap03Ex08 - 명령어 라인에서 서열 정보 입력받기

---
+++ Chap03Ex08.java
@@ -0,0 +1,9 @@
+// Chap03Ex08 - 명령어 라인에서 서열 정보 입력받기
+
+public class Chap03Ex08 {
+    public static void main(String[] args) {
+        String dna_seq = args[0];
+
+        System.out.println(dna_seq);
+    }
+}
