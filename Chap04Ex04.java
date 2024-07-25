7cdec9a50ae83bdfb2c8799c133fe78f5e122aaf
진영 조 <jjojin97@gmail.com>
2024-07-25 11:48:48 +0900

    Ex. Chap03Ex04 - 반복문, while-loop

---
+++ Chap04Ex04.java
@@ -0,0 +1,16 @@
+// Ex. Chap03Ex04 - 반복문, while-loop
+
+public class Chap04Ex04 {
+    public static void main(String[] args) {
+        int i = 0;
+        String dna_seq = "acgtacgt";
+        System.out.println("length: " + dna_seq.length());
+        System.out.println("i - dna");
+        System.out.println("-------");
+        while(i < dna_seq.length()){
+            System.out.println(i + " : " + dna_seq.charAt(i));
+            i++;
+        }
+        System.out.println(i);
+    }
+}
