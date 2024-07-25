a04482816689e3912ffe1350bbbfcfdc9c0d20ef
진영 조 <jjojin97@gmail.com>
2024-07-25 11:44:56 +0900

    DNA 서열의 길이 비교

---
+++ Chap03Ex03.java
@@ -0,0 +1,15 @@
+// Ex. Chap03Ex03 - DNA 서열의 길이 비교
+
+public class Chap03Ex03 {
+    public static void main(String[] args) {
+        String dna_seq1 = "acgtacgt";
+        String dna_seq2 = "acgtagtacgt";
+
+        if(dna_seq1.length() > dna_seq2.length())
+            System.out.println(dna_seq1);
+        else if(dna_seq1.length() < dna_seq2.length())
+            System.out.println(dna_seq2);
+        else
+            System.out.println("same");
+    }
+}
