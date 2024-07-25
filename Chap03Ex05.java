5e984581d64311fc4ccf3e90d269b1d3437f1be1
진영 조 <jjojin97@gmail.com>
2024-07-25 16:16:42 +0900

    Ex. Chap03Ex05 - 반복문으로64가지의 DNA 뉴클리오타이드 생성

---
+++ Chap03Ex05.java
@@ -0,0 +1,20 @@
+// Ex. Chap03Ex05 - 반복문으로64가지의 DNA 뉴클리오타이드 생성
+
+public class Chap03Ex05 {
+    public static void main(String[] args) {
+        String dna = "ACGT";
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                for(int k = 0; k < 4; k++){
+                    System.out.println(
+                        i + " " + j + " " + k + " - " +
+                        dna.charAt(i) + " " +
+                        dna.charAt(j) + " " +
+                        dna.charAt(k)
+                    );
+
+                }
+            }
+        }
+    }
+}
